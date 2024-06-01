97d64055e9ee3c4990b78dce04e8798d2655f1c9
steph-d989 <steb_d@hotmail.com>
2024-06-01 19:25:42 +0200

    pintando los elementos de las paginas secundarias a partir de los botones

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,15 @@
 // VARIABLES
 const listaLibros = document.querySelector('#listaLibros');
-const URL = 'https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=jJgyyVhUzpWhDDqn1MdittMq1PfrAH1q';
+const botonBackArea = document.querySelector('#botonBack')
+const mainIndex = document.querySelector('#mainIndex')
+const URLPrincipal = 'https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=jJgyyVhUzpWhDDqn1MdittMq1PfrAH1q';
+const URLSecundaria = 'https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=jJgyyVhUzpWhDDqn1MdittMq1PfrAH1q';
 const fragment = document.createDocumentFragment();
 
 // EVENTOS 
 document.addEventListener('DOMContentLoaded', () => {
-    llamarAPI()
+
+    llamarAPIPrincipal()
         .then((resp) => {
             pintarCuerpo(resp);
         })
@@ -14,13 +18,26 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 });
 
+document.addEventListener('click', ({ target }) => {
+    if (target.matches('#listaLibros button.botonRead')) {
+        const index = parseInt(target.id);
+        llamarAPISecundaria()
+            .then((resp1) => {
+                pintarSeleccion(resp1,index);
+            })
+            .catch((error1) => {
+                console.log(error1)
+            })
+    }
+});
+
 // FUNCIONES
-const llamarAPI = async () => {
+const llamarAPIPrincipal = async () => {
     try {
-        const response = await fetch(URL);
+        const response = await fetch(URLPrincipal);
         if (response.ok) {
             const data = await response.json();
-            return data.results.lists;
+            return data.results;
         } else {
             throw 'No se encontró URL';
         }
@@ -28,9 +45,23 @@ const llamarAPI = async () => {
         console.error(error);
     }
 };
+const llamarAPISecundaria = async () => {
+    try {
+        const response = await fetch(URLSecundaria)
+        if (response.ok) {
+            const data2 = await response.json();
+            return data2.results.lists;
+        }
+    } catch {
+
+    }
+}
 
 const pintarCuerpo = (resp) => {
-    resp.forEach(element => {
+    const botonBack = document.createElement('button')
+    botonBack.classList.add('botonBack');
+    fragment.append(botonBack);
+    resp.forEach((element, index) => {
         const bookCard = document.createElement('div')
         bookCard.classList.add('bookCard');
 
@@ -41,15 +72,52 @@ const pintarCuerpo = (resp) => {
         const updated = document.createElement('p');
         updated.innerHTML = `Updated: ${element.updated}`;
         const oldest = document.createElement('p');
-        oldest.innerHTML = `Oldest: `;
+        oldest.innerHTML = `Oldest: ${element.oldest_published_date}`;
         const newest = document.createElement('p');
-        newest.innerHTML = `Newest: `
+        newest.innerHTML = `Newest: ${element.newest_published_date}`
         contenidoCard.append(oldest, newest, updated)
-        const botonRead= document.createElement('button');
+        const botonRead = document.createElement('button');
         botonRead.classList.add('botonRead');
-        botonRead.innerHTML=`${'READ MORE!'}<span>${' >'}</span>`;
+        botonRead.innerHTML = `${'READ MORE!'}<span>${' >'}</span>`;
+        botonRead.setAttribute('id', index)
         bookCard.append(nombreLista, contenidoCard, botonRead)
         fragment.append(bookCard)
     });
     listaLibros.append(fragment)
 };
+
+const pintarSeleccion = (resp1, index) => {
+    limpiar(mainIndex);
+    const seccionBoton = document.createElement('section')
+    seccionBoton.setAttribute('id', 'botonBack')
+    const botonBack = document.createElement('button')
+    botonBack.innerHTML = 'BACK TO INDEX'
+    seccionBoton.append(botonBack)
+    const lista = document.createElement('section');
+    lista.setAttribute('id','listaLibros')
+
+    const elementoSeleccionado = resp1[index];
+    (elementoSeleccionado.books).forEach((elemento) => {
+        const cardsRank = document.createElement('div');
+        cardsRank.classList.add('cardsRank');
+        const tituloRank = document.createElement('h5');
+        tituloRank.textContent = `#${elemento.rank} ${elemento.title}`
+        const imagenRank = document.createElement('img');
+        imagenRank.setAttribute('src', elemento.book_image);
+        imagenRank.setAttribute('alt', elemento.title);
+        imagenRank.classList.add('fotoPortada')
+        const descripcion = document.createElement('p');
+        descripcion.textContent = elemento.description;
+        const weeksRank = document.createElement('p')
+        weeksRank.textContent = `Weeks on List: ${elemento.weeks_on_list}`
+        const botonAmazon = document.createElement('button')
+        botonAmazon.textContent = 'BUY AT AMAZON';
+        cardsRank.append(tituloRank, imagenRank, descripcion, botonAmazon);
+        fragment.append(cardsRank);
+    })
+    lista.append(fragment)
+    mainIndex.append(seccionBoton, lista)
+}
+const limpiar = (elemento) => {
+    elemento.innerHTML = '';
+}
